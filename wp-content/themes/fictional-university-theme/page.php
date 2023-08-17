df213027e5067e04440f4b284fb1d3908d3ee8dc
ABHIJITHANU <abhijithsr.personal@gmail.com>
2023-08-17 22:46:41 +0530

    page and childpage

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -12,19 +12,33 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-        </p>
-      </div>
-
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <?php 
+      $theParent = wp_get_post_parent_id(get_the_ID());
+      if ($theParent) { ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+          <p>
+            <a class="metabox__blog-home-link" href="<?=get_permalink($theParent);?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?=get_the_title($theParent);?></a> <span class="metabox__main"><?=the_title();?></span>
+          </p>
+        </div>
+      <?php } ?>
+      
+    <?php 
+    $otherPages = get_pages(['child_of' => get_the_ID()]);
+    if($theParent or $otherPages): 
+    ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?=get_permalink($theParent);?>"><?=get_the_title($theParent);?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <?php 
+            wp_list_pages([
+              'title_li' => NULL,
+              'child_of' => ! empty($theParent) ? $theParent : get_the_ID(),
+              'sort_column' => 'menu_order',
+            ]);
+          ?>
         </ul>
-      </div> -->
+      </div>
+    <?php endif; ?>
 
       <div class="generic-content">
         <?php the_content(); ?>
